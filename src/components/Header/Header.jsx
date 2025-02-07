b6191e40e6b609165065c1313cd8f3e7a3c15339
soheil14 <jalilisoheil14@gmail.com>
2025-02-07 15:44:34 -0800

    add address header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
 import "./Header.css";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaPhoneVolume } from "react-icons/fa6";
+
 const Header = () => {
   return (
     <header>
@@ -10,13 +13,28 @@ const Header = () => {
 const HeaderAdress = () => {
   return (
     <section className="header__adress__main">
-      <div className="header__address">
+      <div className="container">
+        <div className="header__adress__wrapper">
+          <div className="header__address">
+            <img src="/public/assets/icons/location.svg" alt="location" />
+            <span className="header__address-content ">
+              آدرس : شیراز، ملاصدرا، خیابان معدل، ساختمان ۵
+            </span>
+          </div>
+          <div className="header__email">
+            <img src="/public/assets/icons/email.svg" alt="Email" />
+            <span>ایمیل : meymandiarman@gmail.com</span>
+          </div>
+          <div className="header__phone">
+            <FaPhoneVolume color="#fff" width={23} height={23} />
+            <span>شماره تماس و پشتیبانی</span>
 
-    
-
+            <span>
+              <span className="phone__number__first">۰۹۲۱ </span> ۲۳۴ ۴۵۶۶
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="header__email"></div>
-      <div className="header__phone"></div>
     </section>
   );
 };
